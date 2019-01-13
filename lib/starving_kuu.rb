5a62a65661bef9da5128971d939797f7cdd34582
Viet Hoang <hello@vietamins.com>
2019-01-12 18:59:19 -0800

    Add option to run prompt when initiated via command line

--- lib/starving_kuu.rb
+++ lib/starving_kuu.rb
@@ -1,3 +1,7 @@
+run_prompt = ARGV.include?('prompt')
+
 Dir['lib/starving_kuu/*.rb'].each { |file| require_relative(file.delete_prefix('lib/')) }
 
 module StarvingKuu; end
+
+StarvingKuu::Prompt.new.start if run_prompt
