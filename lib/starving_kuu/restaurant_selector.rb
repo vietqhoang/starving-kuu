778fc56e081af11abdddbe85783ead3e72799f4f
Viet Hoang <hello@vietamins.com>
2019-01-13 01:51:12 -0800

    Refactor loading of restaurants
    Create restaurant data file if it does not exist. Expose public method restaurants

--- lib/starving_kuu/restaurant_selector.rb
+++ lib/starving_kuu/restaurant_selector.rb
@@ -7,12 +7,37 @@ module StarvingKuu
   class RestaurantSelector
     RESTAURANT_DATA_ABSOLUTE_PATH = '_data/restaurants.yml'
 
+    attr_accessor :restaurants
+
     def initialize
-      @restaurants = YAML.load_file(RESTAURANT_DATA_ABSOLUTE_PATH)
+      @restaurants = restaurant_data
     end
 
     def sample_restaurant
       @restaurants.sample
+    end
+
+    private
+
+    def restaurant_data
+      load_restaurant_data
+    rescue Errno::ENOENT => e
+      raise unless e.message.include?('No such file or directory')
+
+      create_restaurant_data_file
+      default_restaurant_data
+    end
+
+    def load_restaurant_data
+      YAML.load_file(RESTAURANT_DATA_ABSOLUTE_PATH) || default_restaurant_data
+    end
+
+    def create_restaurant_data_file
+      File.open(RESTAURANT_DATA_ABSOLUTE_PATH, 'a') { |f| f.write(default_restaurant_data.to_yaml) }
+    end
+
+    def default_restaurant_data
+      []
     end
   end
 end
--- spec/lib/starving_kuu/restaurant_selector_spec.rb
+++ spec/lib/starving_kuu/restaurant_selector_spec.rb
@@ -1,7 +1,46 @@
 # frozen_string_literal: true
 
 RSpec.describe StarvingKuu::RestaurantSelector do
+  describe '#restaurants' do
+    context 'when the restaurant data file exists' do
+      context 'and when the restaurant data is empty' do
+        before { allow(YAML).to receive(:load_file).with(described_class::RESTAURANT_DATA_ABSOLUTE_PATH).and_return(YAML.safe_load('')) }
+
+        it 'returns an empty array' do
+          expect(described_class.new.restaurants).to eq([])
+        end
+      end
+
+      context 'and when the restaurant data has content' do
+        let(:restaurants) { ['McDonalds', 'Burger King', 'Wendys', 'Chipotle'] }
+        before { allow(YAML).to receive(:load_file).with(described_class::RESTAURANT_DATA_ABSOLUTE_PATH).and_return(restaurants) }
+
+        it 'returns the array of contents' do
+          expect(described_class.new.restaurants).to contain_exactly(*restaurants)
+        end
+      end
+    end
+
+    context 'when the restaurant data file does not exist' do
+      before { allow(YAML).to receive(:load_file).with(described_class::RESTAURANT_DATA_ABSOLUTE_PATH).and_raise(Errno::ENOENT) }
+
+      it 'returns an empty array' do
+        expect(described_class.new.restaurants).to eq([])
+      end
+
+      it 'creates a restaurant data file' do
+        expect(File).to receive(:open).with(described_class::RESTAURANT_DATA_ABSOLUTE_PATH, 'a')
+
+        described_class.new.restaurants
+      end
+    end
+  end
+
   describe '#sample_restaurant' do
+    let(:restaurants) { ['McDonalds', 'Burger King', 'Wendys', 'Chipotle'] }
+
+    before { allow(YAML).to receive(:load_file).with(described_class::RESTAURANT_DATA_ABSOLUTE_PATH).and_return(restaurants) }
+
     it 'returns a random restaurant' do
       expect(restaurants).to include(described_class.new.sample_restaurant)
     end
