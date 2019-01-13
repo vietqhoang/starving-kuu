4ff7b905603e50d195c5e06bd5a943711c88e525
Viet Hoang <hello@vietamins.com>
2019-01-12 18:36:08 -0800

    Test setup refactor

--- spec/lib/starving_kuu_spec.rb
+++ spec/lib/starving_kuu_spec.rb
@@ -1,5 +1,3 @@
-require File.expand_path('../../lib/starving_kuu.rb', File.dirname(__FILE__))
-
 RSpec.describe StarvingKuu do
   let(:dummy_class) { Class.new { include StarvingKuu } }
 end
---
+++ spec/support/mocks/_data/restaurants.yml.rb
@@ -0,0 +1,20 @@
+module Fixtures
+  module RestaurantHelpers
+    def restaurants
+      [
+        "McDonalds",
+        "Burger King",
+        "Wendys",
+        "Chipotle"
+      ]
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include Fixtures::RestaurantHelpers
+
+  config.before(:each) do
+    allow(YAML).to receive(:load_file).with(StarvingKuu::RestaurantSelector::RESTAURANT_DATA_ABSOLUTE_PATH).and_return(restaurants)
+  end
+end
