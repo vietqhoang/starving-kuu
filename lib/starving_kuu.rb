7cbc989fac7340e3cd87a4614f896aa1b02b3423
Viet Hoang <hello@vietamins.com>
2019-01-12 17:09:12 -0800

    Refactor StarvingKuu
    Seperate responsibility to defined classes

--- lib/starving_kuu.rb
+++ lib/starving_kuu.rb
@@ -1,13 +1,3 @@
-require 'yaml'
+Dir['lib/starving_kuu/*.rb'].each { |file| require_relative(file.delete_prefix('lib/')) }
 
-class StarvingKuu
-  RESTAURANT_DATA_ABSOLUTE_PATH = File.expand_path('../_data/restaurants.yml', File.dirname(__FILE__)).freeze
-
-  def initialize
-    @restaurants = YAML.load_file(RESTAURANT_DATA_ABSOLUTE_PATH)
-  end
-
-  def sample_restaurant
-    @restaurants.sample
-  end
-end
+module StarvingKuu; end
--- spec/lib/starving_kuu_spec.rb
+++ spec/lib/starving_kuu_spec.rb
@@ -1,21 +1,5 @@
 require File.expand_path('../../lib/starving_kuu.rb', File.dirname(__FILE__))
 
 RSpec.describe StarvingKuu do
-  let(:restaurants) { YAML.load_file(File.expand_path('../fixtures/files/restaurants.yml', File.dirname(__FILE__))) }
-
-  before do
-    allow(YAML).to receive(:load_file).with(restaurant_data_absolute_path).and_return(restaurants)
-  end
-
-  describe '#sample_restaurant' do
-    it 'returns a random restaurant' do
-      expect(restaurants).to include(described_class.new.sample_restaurant)
-    end
-  end
-
-  private
-
-  def restaurant_data_absolute_path
-    described_class::RESTAURANT_DATA_ABSOLUTE_PATH
-  end
+  let(:dummy_class) { Class.new { include StarvingKuu } }
 end
